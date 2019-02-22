456d50ea28a105b4f39be4e0c9e56acb4f232fa2
Ranjini11 <47619781+Ranjini11@users.noreply.github.com>
2019-02-22 12:04:53 +0530

    Add files via upload

---
+++ VowelConsonant.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class VowelConsonant{
+public static void main(String[] qwe){
+Scanner scan=new Scanner(System.in);
+System.out.println("enter a char");
+char ch=scan.next().charAt(0);
+if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+System.out.println("VOWEL");
+else{
+System.out.println("CONSONANT");
+}
+}
+}
