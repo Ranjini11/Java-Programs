d97449daf4322eb115f16cbb52dc3df4f60fa517
Ranjini11 <47619781+Ranjini11@users.noreply.github.com>
2019-02-22 12:09:52 +0530

    Add files via upload

---
+++ CheckLargestNumber.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+class CheckLargestNumber{
+public static void main(String[] qwe){
+Scanner scan=new Scanner(System.in);
+int a=scan.nextInt();
+int b=scan.nextInt();
+int c=scan.nextInt();
+if(a>b&&a>c)
+{
+System.out.println("a is greatest");
+}
+else if(b>c)
+{
+System.out.println("b is greatest");
+}
+else if(c>a)
+{
+System.out.println("c is greatest");
+}
+}
+}
