838984992da84232983c10661dcb309598e07d1f
Ranjini11 <47619781+Ranjini11@users.noreply.github.com>
2019-02-22 12:08:22 +0530

    Add files via upload

---
+++ CheckWord.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class CheckWord{
+public static void main(String[] qwe){
+Scanner scan=new Scanner(System.in);
+int N=scan.nextInt();
+int X=1;
+while(X<=N){
+System.out.println("HELLO");
+X++;
+}
+}
+}
