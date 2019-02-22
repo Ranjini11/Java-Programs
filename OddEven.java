67e9717a239a04d9d7583c4c4a8daa2225998abd
Ranjini11 <47619781+Ranjini11@users.noreply.github.com>
2019-02-22 12:06:52 +0530

    Add files via upload

---
+++ OddEven.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class OddEven{
+public static void main(String[] qwe){
+Scanner scan=new Scanner(System.in);
+int a=scan.nextInt();
+if(a%2==0){
+System.out.println("EVEN");
+}
+else
+System.out.println("ODD");
+}
+}
